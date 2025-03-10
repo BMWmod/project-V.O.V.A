6e17e222f6a8bba7fce16d2619d28b291374c088
BMWmod <93707643+BMWmod@users.noreply.github.com>
2025-03-10 18:48:44 +0300

    .

---
+++ components/hero-home.tsx
@@ -0,0 +1,65 @@
+import VideoThumb from "@/public/images/hero-image-01.jpg";
+import ModalVideo from "@/components/modal-video";
+
+export default function HeroHome() {
+  return (
+    <section>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="pb-12 text-center md:pb-20">
+            <h1
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-blue-200),var(--color-gray-50),var(--color-red-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              data-aos="fade-up"
+            >
+              Вова верхний
+            </h1>
+            <div className="mx-auto max-w-3xl">
+              <p
+                className="mb-8 text-xl text-red-200/250"
+                data-aos="fade-up"
+                data-aos-delay={200}
+              >
+                Вова нижний
+              </p>
+              <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
+                <div data-aos="fade-up" data-aos-delay={400}>
+
+
+        
+                </div>
+                <div data-aos="fade-up" data-aos-delay={600}>
+                  <a
+                    className="btn relative w-full bg-linear-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
+                    href="#0"
+                  >
+                    Schedule Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <ModalVideo
+            thumb={VideoThumb}
+            thumbWidth={1104}
+            thumbHeight={576}
+            thumbAlt="Modal video thumbnail"
+            video="videos//video.mp4"
+            videoWidth={1920}
+            videoHeight={1080}
+          />
+          <ModalVideo
+            thumb={VideoThumb}
+            thumbWidth={1104}
+            thumbHeight={576}
+            thumbAlt="Modal video thumbnail"
+            video="videos//video.mp4"
+            videoWidth={1920}
+            videoHeight={1080}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
